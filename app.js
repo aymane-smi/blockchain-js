c4a246762c9f19f4c24d08a3df701e03c22a6962
aymane <aymanebel2@outlook.fr>
2022-03-07 01:12:37 +0100

    blockchain

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express    = require("express"),
       app        = express(),
       PORT       = process.argv[2] || 8081,
       routes     = require("./routes/index"),
-      bodyParser = require("body-parser");
+      bodyParser = require("body-parser"),
+      cors       = require("cors");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 app.use("/api/", routes);
 app.listen(PORT, ()=>{
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const Blockchain        = require('../dev/blockchain'),
       bitcoin           = new Blockchain(),
-      {v4 : uuid}       = require('uuid');
+      {v4 : uuid}       = require('uuid'),
+      rp                = require("request-promise");
+    //   fetch           = require("node-fetch");
+    //   {fetch}           = require("undici");
+      
       
 exports.Blockchain = (req, res)=>{
-    console.log("inside controllers:", process.argv[2]);
     res.json({
         message: "blockchain",
         bitcoin 
@@ -40,76 +43,71 @@ exports.Mine = (req, res)=>{
     const nodeAddress = uuid().split('-').join('');
     bitcoin.createNewTransaction(6.25, '00', nodeAddress);
     res.status(200).json({
-        message: "new Block mined Mine",
+        message: "new Block mined ",
         newBlock
     });
 };
 
 
 exports.Register_n_Broadcast = async(req, res)=>{
-    try{
         const {newNodeUrl} = req.body;
         let URL;
         let options;
         if(bitcoin.networkNodes.indexOf(newNodeUrl) === -1)
             bitcoin.networkNodes.push(newNodeUrl);
         const registred_nodes = [];
+        
         for(node of bitcoin.networkNodes){
-            URL = `${node}/register-node`;
+            URL = `${node}/api/register-node`;
             options = {
+                uri: URL,
                 method: 'POST',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body:{
+                body: {
                     newNodeUrl
-                }
+                },
+                json: true
             };
-            const {data}  = await fecth(URL, options);
-            if(data)
-                registred_nodes.push(data);
+            registred_nodes.push(rp(options));
         }
-        URL  = `${newNodeUrl}/register-nodes-bulk`;
-        option = {
-            method: "POST",
-            headers: {
-                "Content-Type" : "application/json"
-            },
-            body : {
-                allNetworkNodes : [...bitcoin.networkNodes, bitcoin.currentNodeUrl]
-            }
-        };
-        let {data} = await fetch(URL, options);
-        if(data)
-            res.status(200).json({
-                message: "New Node registered with network successfully"
+        Promise.all(registred_nodes).then(data=>{
+            URL = `${newNodeUrl}/api/register-nodes-bulk`;
+            const options = {
+                uri: URL,
+                method: 'POST',
+                body : {
+                    allNetworkNodes : [...bitcoin.networkNodes, bitcoin.currentNodeUrl]
+                },
+                json: true
+            };
+            return rp(options);
+        })
+        .then(data=> res.status(200).json("New Node registered with network successfully"))
+        .catch(err=>{
+            res.status(500).json({
+                message: err.message
             });
-    }catch(err){
-        res.status(500).json({
-            message: "something wen't wrong!",
-            detail: err.message
         });
-    }
 };
 
 
 exports.RegisterNode = (req, res)=>{
-
-    const {newNodeUrl} = req.body;
-    if(!newNodeUrl)
-        res.status(404).json({
-            message: "NodeUrl not found"
-        });
-    const notExist = bitcoin.networkNodes.indexOf(newNodeUrl) === -1;
-    const existantUrl = bitcoin.currentNodeUrl !== newNodeUrl;
-    if(notExist && existantUrl){
-        bitcoin.networkNodes.push(newNodeUrl);
+    try{
+        const {newNodeUrl} = req.body;
+        if(!newNodeUrl)
+            res.status(404).json({
+                message: "NodeUrl not found"
+            });
+        const notExist = bitcoin.networkNodes.indexOf(newNodeUrl) === -1;
+        const existantUrl = bitcoin.currentNodeUrl !== newNodeUrl;
+        if(notExist && existantUrl)
+            bitcoin.networkNodes.push(newNodeUrl);
         res.status(200).json({
             message: "New node registered successfully."
         });
-    }else{
-        res.status(406).json({
-            message: "NodeUrl can't be added."
+    }catch(err){
+        res.status(500).json({
+            message: "something wen't wrong!",
+            deatil: err.message
         });
     }
 };
@@ -117,18 +115,16 @@ exports.RegisterNode = (req, res)=>{
 exports.RegisterNodeBulk = (req, res)=>{
     try{
         const {allNetworkNodes} = req.body;
+        let key = true;
         for(networkNode of allNetworkNodes){
             let notExist = bitcoin.networkNodes.indexOf(networkNode) === -1;
             let existantUrl = bitcoin.currentNodeUrl !== networkNode;
-            if(!notExist || !existantUrl)
-            res.status(406).json({
-                message: "can't be bulked"
-            });
-            if(notExist && existantUrl)
+            if(notExist && existantUrl){
                 bitcoin.networkNodes.push(networkNode);
+            }
         }
         res.status(200).json({
-            message: "Bulk registration successful"
+            message: "Bulk registration successful."
         });
     }catch(err){
         console.log(err);
